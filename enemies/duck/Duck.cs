c6e2544b227e57c6a9ad1fdc5ecda2585cd593eb
GameBuilder101 <gamebuilder101@gmail.com>
2024-01-27 17:55:18 -0500

    Added stuff

---
+++ enemies/duck/Duck.cs
@@ -0,0 +1,63 @@
+using Godot;
+using System;
+
+// BEHOLD, FOR YOU GAZE UPON A GOD
+public partial class Duck : Node2D
+{
+    public bool IsAwake { get; private set; }
+    [Export]
+    private Node2D _sleepingSprite;
+    [Export]
+    private Node2D _awakeSprite;
+
+    private Vector2 _startPosition;
+
+    [Export]
+    private float _speed = 100.0f;
+    [Export]
+    private Node2D _chasing;
+    [Export]
+    private CollisionShape2D _killTrigger;
+
+    public override void _Ready()
+	{
+        AddToGroup(Level.ResetableGroup);
+
+        _startPosition = Position;
+    }
+
+	public override void _Process(double delta)
+	{
+        if (IsAwake)
+        {
+            Position = Position.MoveToward(_chasing.Position, _speed * (float)delta); // Impending doom
+            _awakeSprite.Scale = new Vector2(_chasing.Position.X < Position.X ? -1.0f : 1.0f, 1.0f);
+        }
+    }
+
+    private void _on_awake_trigger_body_entered(Node2D body)
+    {
+        if (body is Player)
+            Awake();
+    }
+
+    public void Awake()
+    {
+        if (IsAwake)
+            return;
+        IsAwake = true;
+        _sleepingSprite.Hide();
+        _awakeSprite.Show();
+        _killTrigger.SetDeferred("disabled", false);
+    }
+
+    public void Reset(int newDeathCount)
+    {
+        Position = _startPosition;
+
+        IsAwake = false;
+        _sleepingSprite.Show();
+        _awakeSprite.Hide();
+        _killTrigger.SetDeferred("disabled", true);
+    }
+}
---
+++ level/SlideVisualTrigger.cs
@@ -0,0 +1,54 @@
+using Godot;
+using System;
+
+public partial class SlideVisualTrigger : Area2D
+{
+	[Export]
+	public Node2D slideTarget;
+	[Export]
+	public Node2D targetPosition;
+	[Export]
+	public double duration;
+    private double _currentSlideTime = -1.0;
+    /// <summary>
+    /// When true, the trigger will reset on death.
+    /// </summary>
+    [Export]
+    public bool reset = true;
+
+    private Vector2 _startPosition;
+
+    public override void _Ready()
+	{
+        AddToGroup(Level.ResetableGroup);
+        BodyEntered += OnTrigger;
+        _startPosition = slideTarget.Position;
+    }
+
+    public override void _Process(double delta)
+    {
+        base._Process(delta);
+        if (_currentSlideTime >= 0.0)
+        {
+            _currentSlideTime += delta;
+            if (_currentSlideTime > duration)
+                _currentSlideTime = duration;
+            slideTarget.Position = _startPosition.Lerp(targetPosition.Position, (float)(_currentSlideTime / duration));
+        }
+    }
+
+    private void OnTrigger(Node2D body)
+    {
+        if (_currentSlideTime >= 0.0 || !(body is Player))
+            return;
+        _currentSlideTime = 0.0;
+    }
+
+    public void Reset(int newDeathCount)
+    {
+        if (!reset)
+            return;
+        _currentSlideTime = -1.0;
+        slideTarget.Position = _startPosition;
+    }
+}
