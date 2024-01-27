d49dc98facaec48ddfca7f774147fe3965dcde4a
Lyx Huston <lyx@hustonfam.net>
2024-01-27 12:20:29 -0500

    make test work with new changes

--- UI/narrator/test_narrator_scripts/TestScript.cs
+++ UI/narrator/test_narrator_scripts/TestScript.cs
@@ -9,7 +9,7 @@ public partial class TestScript : Node
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		narratorScript.StartOver();
+		narratorScript.Start();
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
