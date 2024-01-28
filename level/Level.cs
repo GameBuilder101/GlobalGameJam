96d25bdd332b9a8d5e986833f7bd41900b24d24a
GiovanniDanilsoPaulino <149913810+GiovanniDanilsoPaulino@users.noreply.github.com>
2024-01-28 15:20:50 -0500

    Fix the ceiling

--- level/Level.cs
+++ level/Level.cs
@@ -44,7 +44,7 @@ public partial class Level : Node2D
 		if (_currentDeathDialogue < deathDialogues.Count && !NarratorTextLabel.Self.IsWriting)
 		{
 			if (deathDialogues[_currentDeathDialogue] != null)
-                deathDialogues[_currentDeathDialogue].Start();
+				deathDialogues[_currentDeathDialogue].Start();
 			_currentDeathDialogue++;
 		}
 	}
@@ -55,53 +55,53 @@ public partial class Level : Node2D
 		GetNode<AudioStreamPlayer>("GoofballMarch").Play();
 	}
 
-    private void _on_duck_theme_1_finished()
-    {
-        GetNode<AudioStreamPlayer>("DuckTheme2").Play();
-    }
+	private void _on_duck_theme_1_finished()
+	{
+		GetNode<AudioStreamPlayer>("DuckTheme2").Play();
+	}
 
-    private void _on_duck_theme_2_finished()
-    {
-        GetNode<AudioStreamPlayer>("DuckTheme2").Play();
-    }
+	private void _on_duck_theme_2_finished()
+	{
+		GetNode<AudioStreamPlayer>("DuckTheme2").Play();
+	}
 
-    /// <summary>
-    /// Resets the level and any resetables.
-    /// </summary>
-    public void Reset()
+	/// <summary>
+	/// Resets the level and any resetables.
+	/// </summary>
+	public void Reset()
 	{
 		GetTree().CallGroup(ResetableGroup, "Reset", player.DeathCount);
 	}
 
-    public void _on_checkpoint_slide_trigger_body_entered(Node2D body)
-    {
+	public void _on_checkpoint_slide_trigger_body_entered(Node2D body)
+	{
 		if (_playedSlide)
 			return;
 		_playedSlide = true;
-        GetNode<AudioStreamPlayer>("CheckpointSlide").Play();
-    }
+		GetNode<AudioStreamPlayer>("CheckpointSlide").Play();
+	}
 
-    public void _on_duck_reveal_trigger_body_entered(Node2D body)
-    {
-        GetNode<AudioStreamPlayer>("DuckReveal").Play();
-    }
-
-    private void _on_checkpoint_trigger_body_entered(Node2D body)
+	public void _on_duck_reveal_trigger_body_entered(Node2D body)
 	{
-        FadeAudioStreamPlayer(GetNode<AudioStreamPlayer>("GoofballMarch"));
-    }
+		GetNode<AudioStreamPlayer>("DuckReveal").Play();
+	}
 
-    public void _on_duck_theme_1_trigger_body_entered(Node2D body)
-    {
+	private void _on_checkpoint_trigger_body_entered(Node2D body)
+	{
+		FadeAudioStreamPlayer(GetNode<AudioStreamPlayer>("GoofballMarch"));
+	}
+
+	public void _on_duck_theme_1_trigger_body_entered(Node2D body)
+	{
 		GetNode<AudioStreamPlayer>("DuckTheme1").Play();
-    }
+	}
 
-    public void _on_duck_theme_2_trigger_body_entered(Node2D body)
-    {
-        FadeAudioStreamPlayer(GetNode<AudioStreamPlayer>("DuckTheme2"));
-    }
+	public void _on_duck_theme_2_trigger_body_entered(Node2D body)
+	{
+		FadeAudioStreamPlayer(GetNode<AudioStreamPlayer>("DuckTheme2"));
+	}
 
-    private void FadeAudioStreamPlayer(AudioStreamPlayer player)
+	private void FadeAudioStreamPlayer(AudioStreamPlayer player)
 	{
 		Tween tween = CreateTween();
 		tween.TweenProperty(player, "volume_db", -80.0f, 3.0f);
